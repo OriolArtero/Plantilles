30a4fe1d4e37a9015eb9741905ad7cff43d1a7e8
OriolArtero <Jackesg_92@hotmail.com>
2015-01-30 20:44:19 +0100

    primera practica
    practica arduino primera practica

---
+++ Primera_practica/Primera_practica.ino
@@ -0,0 +1,42 @@
+/******************************************************************************
+**                                                                           **
+**                         Descripció programa                               **
+******************************************************************************/
+
+//******  Includes  ***********************************************************
+
+//******  Variables  **********************************************************
+
+//******  Setup  **************************************************************
+void setup(){
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+
+}
+
+
+//******  Loop  ***************************************************************
+void loop (){
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);              // wait for a second
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);              // wait for a second digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);              // wait for a second
+   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);              // wait for a second
+   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second 
+  
+}
